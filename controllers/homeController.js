bc81cbb99ac475bf688ee270d476b348db87ec02
NicoCodesCode <nicolasrmz08@gmail.com>
2025-03-22 08:24:24 -0500

    add error handling

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,13 @@
 const { getAllFolders, getAllFiles } = require("../prisma/queries");
 
-const renderHomePage = async (req, res) => {
-  res.render("home", {
-    title: "File Uploader",
-    folders: await getAllFolders(),
-    files: await getAllFiles(),
-  });
+const renderHomePage = async (req, res, next) => {
+  try {
+    const folders = await getAllFolders();
+    const files = await getAllFiles();
+    res.render("home", { title: "File Uploader", folders, files });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { renderHomePage };
